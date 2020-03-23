f067ef87969d57fbe94a69d2e3cfdbc2eeafd7d5
Galih Ariwanda <nyamuk.pc01@gmail.com>
2020-03-23 12:22:33 +0700

    Updates

---
+++ precache-manifest.091da7b6c77c8d214a7bdb8d9805aa68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfbd1bd4c50fe956da32547855ac20f8",
+    "url": "/example/index.html"
+  },
+  {
+    "revision": "652ebd5b33c459d3e8b2",
+    "url": "/example/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "46f1821916b39b8f2da7",
+    "url": "/example/static/js/2.95a30129.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/example/static/js/2.95a30129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "652ebd5b33c459d3e8b2",
+    "url": "/example/static/js/main.b8281782.chunk.js"
+  },
+  {
+    "revision": "e51a8cc5f1d3bf7f15a3",
+    "url": "/example/static/js/runtime-main.00e6265a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/example/static/media/logo.5d5d9eef.svg"
+  }
+]);
